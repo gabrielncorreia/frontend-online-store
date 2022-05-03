b3f9b55ec7b67dcc84247f3402c528cd062b717e
Gabriel <gncorreia@protonmail.com>
2022-05-03 13:41:02 -0300

    Updates

---
+++ precache-manifest.38def3bb4269f7112c4176d2797dff31.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "aa6dd278a03ee26d8d038b46a3e81404",
+    "url": "/frontend-online-store/index.html"
+  },
+  {
+    "revision": "01ea8a6f817c64933498",
+    "url": "/frontend-online-store/static/css/main.cf3e4ace.chunk.css"
+  },
+  {
+    "revision": "c8632e8aba14f1ce97f8",
+    "url": "/frontend-online-store/static/js/2.bc21603b.chunk.js"
+  },
+  {
+    "revision": "88a653695d6281b45c0dc6f975f8f46b",
+    "url": "/frontend-online-store/static/js/2.bc21603b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "01ea8a6f817c64933498",
+    "url": "/frontend-online-store/static/js/main.c40a300a.chunk.js"
+  },
+  {
+    "revision": "582737c6385ae9ed11b6",
+    "url": "/frontend-online-store/static/js/runtime-main.720530ed.js"
+  }
+]);
